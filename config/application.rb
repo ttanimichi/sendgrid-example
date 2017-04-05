6358125087b8bf7b9ca5f3a3c9a1c8aa7c7fac48
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-04-05 18:45:17 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,37 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SendgridExample
+  class Application < Rails::Application
+    
+    config.time_zone = 'Tokyo'
+
+    config.generators do |generators|
+      generators.assets false
+      generators.helper false
+      generators.stylesheets false
+      generators.javascripts false
+    end
+
+    config.autoload_paths << "#{config.root}/app/services"  
+  
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
