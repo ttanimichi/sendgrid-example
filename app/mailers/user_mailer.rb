71e2c15d273c5fb7366afe5cd746aac975cad610
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-04-06 19:56:17 +0900

    Specify a category

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,9 @@
 class UserMailer < ApplicationMailer
+  include SendGrid
+
   def welcome_email
+    sendgrid_category 'MM-test01'
+
     @title = 'hello'
     mail(to: ENV['TO'], subject: 'Hello')
   end
