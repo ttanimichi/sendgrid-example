deed72d4e05914d145ddd516df311c88a598aedf
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-04-06 18:54:44 +0900

    Add smtp_settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['USER_NAME'],
+  password:  ENV['PASSWORD'],
+  domain:    ENV['DOMAIN'],
+  address:   ENV['ADDRESS'],
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
