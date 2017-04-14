03490e7039b82a7309a67876b92d5a3e40b8dc41
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-04-14 15:47:01 +0900

    Add digest auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  USERS = { ENV.fetch('DG_USER') => ENV.fetch('DG_PASS') }
+
+  before_action :authenticate
+
+  private
+
+  # TODO: concern に追い出す
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
